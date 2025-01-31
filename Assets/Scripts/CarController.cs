725ed457031103e9f9d940a89cf893b23e90c6a7
ESMA-NicolasR <n.richard@rns.ecolescreatives.com>
2025-01-31 22:49:55 +0100

    tweaking

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,24 +6,27 @@ using UnityEngine.Serialization;
 
 public class CarController : MonoBehaviour
 {
-    [Header("Steering settings")] 
     private Rigidbody _rb;
+    [Header("Steering settings")] 
     public float steering;
     
     [Header("Acceleration settings")] 
-    private float _acceleration;
     [SerializeField] private AnimationCurve _accelerationCurve;
     [SerializeField] private float _maxAcceleration;
     [SerializeField] private float _timeToAccelerate;
     [SerializeField] private float _deceleration;
+    [SerializeField] private float _timeToDecay;
+    private float _acceleration;
     private float _accelerationInterpolator;
     
     [Header("Speed settings")]
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _minSpeed;
     private float _speed;
-    public float maxSpeed;
     
     [Header("Power up settings")]
     [SerializeField] private float _mushroomIncreasedSpeed;
+    [SerializeField] private float _mushroomDecayTime;
     private bool isBoosting;
 
     public Vector2 directionInput;
@@ -36,30 +39,29 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        directionInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        directionInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         if (Input.GetKeyDown(KeyCode.Space) && !isBoosting)
         {
-            StartCoroutine(Boost());
+            StartCoroutine(Boost(_mushroomIncreasedSpeed, _mushroomDecayTime));
         }
         
     }
 
-    public IEnumerator Boost()
+    public IEnumerator Boost(float speedIncrease, float decayTime)
     {
         isBoosting = true;
-        var decayTime = 1.0f;
-        var prevMaxSpeed = maxSpeed;
-        var newMaxSpeed = maxSpeed*10.0f;
-        maxSpeed = newMaxSpeed;
+        var prevMaxSpeed = _maxSpeed;
+        var newMaxSpeed = _maxSpeed*speedIncrease;
+        _maxSpeed = newMaxSpeed;
         _rb.velocity = transform.forward * newMaxSpeed;
         while (decayTime > 0)
         {
-            maxSpeed = Mathf.Lerp(prevMaxSpeed, newMaxSpeed, decayTime);
+            _maxSpeed = Mathf.Lerp(prevMaxSpeed, newMaxSpeed, decayTime);
             decayTime -= Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
-        maxSpeed = prevMaxSpeed;
+        _maxSpeed = prevMaxSpeed;
         isBoosting = false;
     }
     
@@ -74,21 +76,35 @@ public class CarController : MonoBehaviour
         zVelocity = Mathf.Clamp(zVelocity, -maxSpeed, maxSpeed);
         localVelocity = new Vector3(xVelocity, yVelocity, zVelocity);
         _rb.velocity = _rb.transform.TransformDirection(localVelocity);*/
-        // Steering
+        // Look up and down
         var xAngle = transform.eulerAngles.x;
         if (xAngle > 180) xAngle -= 360;
         xAngle = Mathf.Clamp(xAngle, -40, 40);
         var yAngle = transform.rotation.eulerAngles.y;
         var zAngle = 0f;
         transform.eulerAngles = new Vector3(xAngle, yAngle, zAngle);
+        // Steering
+        //_rb.AddTorque(directionInput.x * steering * transform.up, ForceMode.Acceleration);
         _rb.transform.eulerAngles += directionInput.x * steering * Time.fixedDeltaTime * transform.up;
         
         // Moving
-        _accelerationInterpolator += directionInput.y * Time.fixedDeltaTime/_timeToAccelerate;
+        _accelerationInterpolator += directionInput.y * Time.fixedDeltaTime / _timeToAccelerate;
         _accelerationInterpolator = Mathf.Clamp01(_accelerationInterpolator);
-        _acceleration = _accelerationCurve.Evaluate(_accelerationInterpolator)*_maxAcceleration;
+        if (directionInput.y > 0)
+        { // Accelerate
+            _acceleration = _accelerationCurve.Evaluate(_accelerationInterpolator) * _maxAcceleration;
+        }else if (directionInput.y < 0)
+        { // Decelerate
+            _acceleration = -_deceleration;
+        }
+        else
+        { // Natural drag
+            _acceleration = 0f;
+            _accelerationInterpolator -= Time.fixedDeltaTime/_timeToDecay;
+        }
+
         _rb.velocity += _acceleration * Time.fixedDeltaTime * transform.forward;
-        _rb.velocity = transform.forward * Mathf.Clamp(_rb.velocity.magnitude, 0f, maxSpeed);
+        _rb.velocity = transform.forward * Mathf.Clamp(_rb.transform.InverseTransformDirection(_rb.velocity).z, _minSpeed, _maxSpeed);
         
         
         //_rb.MovePosition(transform.position + transform.forward * _speed * Time.fixedDeltaTime);
@@ -99,6 +115,6 @@ public class CarController : MonoBehaviour
         GUILayout.Label($"x: {_rb.velocity.x:F}, y: {_rb.velocity.y:F}, z: {_rb.velocity.z:F}");
         var localVelocity = _rb.transform.InverseTransformDirection(_rb.velocity);
         GUILayout.Label(localVelocity.ToString());
-        GUILayout.Label($"_speed : {_speed:F}, _acceleration : {_acceleration:F}");
+        GUILayout.Label($"_speed : {_speed:F}, _acceleration : {_acceleration:F}, _accelerationInterpolator : {_accelerationInterpolator:F}");
     }
 }
